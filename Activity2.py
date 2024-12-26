33b0a602919d92a57b08f9140ee959f56757c011
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-26 14:25:27 +0600

    sswsssss

---
+++ Activity2.py
@@ -0,0 +1,27 @@
+x = 69
+y = 69
+z = 420
+
+print (x != z)
+print (y != z)
+
+lang = "Javascript"
+act = "Coding"
+
+if lang != act:
+    print(f"{lang} and {act} are different")
+
+ping = 200
+recping = 90
+
+if (ping == 100) != (recping == 90):
+    print("the game is very laggy")
+
+num = int(input("Enter A Number: "))
+
+if (num % 2) != 0:
+    print(f"{num} is an odd number")
+
+else:
+    print(f"{num} is an odd number")
+
